702ee63543e15acfbdbca8f549211042c3e832a5
BANDU-OF-TG <87604582+jithin123455@users.noreply.github.com>
2021-09-19 19:35:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/kalyaniv2/43357")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
